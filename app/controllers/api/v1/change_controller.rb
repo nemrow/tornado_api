1e8a35cf2541d1a186ba3c693b50afc44ff36678
Nemrow <jnemrow@shopittome.com>
2014-08-28 14:04:44 -0700

    Adjust for image content types

--- app/controllers/api/v1/change_controller.rb
+++ app/controllers/api/v1/change_controller.rb
@@ -4,8 +4,14 @@ module Api
       def create
         page = Page.find_or_create_by_url(params[:url])
         change = Change.find_or_create_by_page_id_and_selector(page.id, params[:selector])
-        change.update_attributes(:content => params[:content], :content_type => params[:content_type])
-        render :nothing => true
+        if params[:content_type] == "image"
+          file_path = "test_images_#{Time.now.to_i}.jpg"
+          AwsHelper.write_to_aws(file_path, params[:content].tempfile)
+          change.update_attributes(:content => "#{ENV['AWS_BUCKET_PATH']}/#{file_path}", :content_type => "image")
+        elsif params[:content_type] == "text"
+          change.update_attributes(:content => params[:content], :content_type => "text")
+        end
+        render :json => page.changes
       end
     end
   end
