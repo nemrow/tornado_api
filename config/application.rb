144510c4ec2d675239ffe010edd770cfd985a3e6
Nemrow <nemrowj@gmail.com>
2014-09-06 13:54:02 -0700

    precompile bug fix

--- config/application.rb
+++ config/application.rb
@@ -65,6 +65,8 @@ module TornadoApi
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
 
+    config.assets.initialize_on_precompile = false
+
     config.generators do |g|
       g.stylesheets = false
       g.javascripts = false
